9835942342b7323dbb9b6a12f2ab9d480d33fd88
wogus1228 <dlwogus1228@naver.com>
2021-02-08 22:42:06 +0900

    Update 모험가 길드.py

--- 코딩테스트/그리디 알고리즘/모험가 길드.py
+++ 코딩테스트/그리디 알고리즘/모험가 길드.py
@@ -20,3 +20,17 @@ for i in data:
 print(cnt)
 
 # 해답
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+result = 0
+count = 0
+
+for i in data:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0
+
+print(result)
