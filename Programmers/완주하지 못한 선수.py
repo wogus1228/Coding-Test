f27235184a43de912c848d2fd002b83c66768f5b
wogus1228 <dlwogus1228@naver.com>
2022-06-15 16:15:38 +0900

    Update 완주하지 못한 선수.py

--- Programmers/완주하지 못한 선수.py
+++ Programmers/완주하지 못한 선수.py
@@ -1,17 +1,17 @@
 def solution(participant, completion):
     answer = ''
-    parts = {part: 0 for part in participant}
+    parts = {part: 0 for part in participant} # 딕셔너리 생성
     
     for part in participant:
         if part in parts:
-            parts[part] += 1
+            parts[part] += 1 # 참가자 명단에 존재할 시 체크
     
     for comp in completion:
         if comp in parts:
-            parts[comp] -= 1
+            parts[comp] -= 1 # 완주자 명단에 존재할 시 체크
     
     for key, value in parts.items():
-        if value != 0:
-            answer = key
+        if value != 0: # 체크안된 대상 확인
+            answer = key 
     
     return answer
